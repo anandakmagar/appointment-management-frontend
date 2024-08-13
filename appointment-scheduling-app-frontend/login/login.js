9ed4ede301106be069cbbcffa03e66e7dc84dfe8
Ananda Magar <anandakmagar@gmail.com>
2024-08-12 20:48:04 -0600

    logout feature modified

--- appointment-scheduling-app-frontend/login/login.js
+++ appointment-scheduling-app-frontend/login/login.js
@@ -1,42 +1,3 @@
-function logoutUser() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('refreshToken');
-
-    fetch('https://appointment-management-da90d3c8d8ca.herokuapp.com/security/logout', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-    }).then(response => {
-        if (response.ok) {
-            console.log('Logged out successfully');
-        } else {
-            console.log('Failed to log out');
-        }
-    }).finally(() => {
-        window.location.href = 'login.html';
-    });
-}
-
-function checkTokenExpiration() {
-    const token = localStorage.getItem('token');
-    if (!token) {
-        logoutUser();
-        return;
-    }
-
-    const payload = JSON.parse(atob(token.split('.')[1]));
-    const expirationTime = payload.exp * 1000;
-    const currentTime = Date.now();
-
-    if (currentTime >= expirationTime) {
-        logoutUser();
-    }
-}
-
-
-
 
 document.getElementById('loginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
@@ -67,12 +28,3 @@ document.getElementById('loginForm').addEventListener('submit', async function (
         alert('An error occurred while logging in.');
     }
 });
-
-// Checking token expiration on page load
-window.addEventListener('load', checkTokenExpiration);
-
-// Also, checking token expiration at regular intervals (every minute)
-setInterval(checkTokenExpiration, 60000);
-
-// Triggering logout manually
-document.getElementById('logoutButton').addEventListener('click', logoutUser);
